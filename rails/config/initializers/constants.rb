12ddcdae93e31796a258606a0745b8533a168856
Artem Kholodenko <artemk@gmail.com>
2015-04-30 23:22:49 -0700

    staying sign up

--- rails/config/initializers/constants.rb
+++ rails/config/initializers/constants.rb
@@ -1,8 +1,7 @@
-BOOTSTRAP_CSS = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css"
-BOOTSTRAP_JS = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"
+BOOTSTRAP_CSS = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css"
+BOOTSTRAP_JS = "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"
 JQUERY_CDN = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'
-ANGULAR_CDN = '//ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js'
-ANGULAR_1_4_CDN = '//ajax.googleapis.com/ajax/libs/angularjs/1.4.0-rc.0/angular.min.js'
+ANGULAR_CDN = '//ajax.googleapis.com/ajax/libs/angularjs/1.4.0-rc.0/angular.min.js'
 ANGULAR_UI_ROUTER_CDN = "//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.10/angular-ui-router.min.js"
 ANGULAR_UI_BOOTSTRAP_CDN = "//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js"
 FONT_AWESOME_CSS = "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
